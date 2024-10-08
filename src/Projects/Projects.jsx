381d00188051014fdd0927690d439fc088c729b6
Raam <raam.sanghani@gmail.com>
2024-10-08 12:46:53 -0500

    Set up data points, now planning the infrastructure. I'm going to save the opposing horizontal scroll sections for another day, and start with simply doing one. Eventually, I'll utilize an overarching active projects section to have a bit more flexibility over when certain actions trigger based on which project's info is being transitioned into. Maybe. But next immediate step is to start setting everything up vis a vis actual thumbnails or models, and then utilising them in the set up to see how it looks.

--- src/Projects/Projects.jsx
+++ src/Projects/Projects.jsx
@@ -4,12 +4,17 @@ import Title from './Title/Title'
 import SkillCard from './SkillCard/SkillCard'
 import ModalInfo from './ModalInfo/ModalInfo'
 
+import data from '../utils/data.js'
+
 
 function Projects() {
     /**
      * This will house the overall layout of the Projects Section
      * I think it should also compile each child component and we should have this component solely handle gsap logic for horizontal scroll fnctlty
      */
+
+
+
     return (
         <section className='projects-section'>
             Projects
---
+++ src/utils/data.js
@@ -0,0 +1,37 @@
+const data = {
+    portfolio: {
+        title: "My Portfolio",
+        thumbnail: "thumbnail",
+        model: "gltf path",
+        description: "This is my portfolio project, built with the React JS and React Three Fiber libraries.",
+        links: {github: 'https://github.com/Raamtaro/Readme-For-Recruiters', live: 'Netlify Link coming soon.'},
+        skills: ['javascript', 'react', 'three js', 'webGL']
+    },
+    messenger: {
+        title: "Messenger",
+        thumbnail: "thumbnail",
+        model: "gltf path",
+        description: "This is the messenger project...",
+        links: {github: 'https://github.com/Raamtaro/RaamtaroMessengerServer', live: 'Netlify Link coming soon.'},
+        skills: ['javascript', 'react', 'node js', 'express', 'pSQL', 'prisma']
+    },
+    threePortfolio: {
+        title: "WebGL Project Showcase",
+        thumbnail: "thumbnail",
+        model: "gltf path",
+        description: "Coming soon...",
+        links: {github: 'coming soon...', live: 'Netlify Link coming soon.'},
+        skills: ['javascript', 'three js', 'webGL']
+    },
+    gallery: {
+        title: "Photo Gallery",
+        thumbnail: "thumbnail",
+        model: "gltf path",
+        description: "This is the Photo Gallery project....",
+        links: {github: 'https://github.com/Raamtaro/PhotoExhibitor', live: 'Netlify Link coming soon.'},
+        skills: ['javascript', 'three js', 'webGL', 'react', 'node js', 'express', 'pSQL', 'prisma']
+    }
+    
+}
+
+export default data
